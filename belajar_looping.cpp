b9c8800afa62352e5873ddfa53ab6c5aabecaa58
nicomaloky <114230875+nicomaloky@users.noreply.github.com>
2022-12-01 09:46:07 +0700

    belajar_looping

---
+++ belajar_looping.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+int mian(){
+
+    //for
+    for (int i=0; i<= 5; i++)
+
+{
+    cout<< "perulangan for : "<< i <<endl;
+}
+    //while
+    int x;
+    while (x<=7)
+{
+    cout<< "perulangan while : "<< x <<endl;
+    x++;
+}
+    //do while
+    int y;
+    do{
+     cout<< "perulangan do while ke -"<< y << endl;
+     y++;   
+}while(y<=3);
+
+
+
+
+}
