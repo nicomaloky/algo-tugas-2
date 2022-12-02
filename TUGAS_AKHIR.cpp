bacabc3f121251938c9781167d63f202ecc8b4cd
nicomaloky <114230875+nicomaloky@users.noreply.github.com>
2022-12-02 10:47:51 +0700

    tugas-akhir

---
+++ TUGAS_AKHIR.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    int a;
+
+    cin >> a;
+    if( a >= 5 ){
+        cout<< "kontol"<<endl;
+    }
+    else{
+        cout<< "ah ah ah"<<endl;
+
+    }
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
